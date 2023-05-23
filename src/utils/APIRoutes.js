892f118fffe08c06392652120df83b1ef515c7a1
Abhishek Rauthan <abhishekrauthan733@gmail.com>
2023-05-23 12:06:20 +0530

    git commit -m "d"

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://mern-chatapp-backend.vercel.app";
+export const host = "https://mern-chatapp-backend.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
